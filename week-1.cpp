5c38557551cc9f200d14a219c162da924ad13a1f
msahin4 <mstfsahin14@gmail.com>
2023-10-19 22:39:01 +0300

    week-1 added.

---
+++ week-1.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main () {
+	
+	printf("Hello world !");
+	
+	int a, b, toplam;
+	
+	printf("\nBirinci sayiyi giriniz");
+	scanf("%d",&a);
+	printf("\nBirinci sayiyi giriniz");
+	scanf("%d",&b);
+	
+	toplam = a + b;
+	printf("Toplam = %d",toplam);
+	
+	
+	return 0;
+}

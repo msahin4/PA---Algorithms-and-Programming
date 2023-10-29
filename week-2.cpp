f9974ece27f1c3fd145c87a313552b25c2a4fa1b
msahin4 <115533661+msahin4@users.noreply.github.com>
2023-10-29 23:00:36 +0300

    Add files via upload
    Signed-off-by: msahin4 <115533661+msahin4@users.noreply.github.com>

---
+++ week-2.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	
+	int x,y,z,sonuc;
+	printf("Uc tam sayi giriniz... \n");
+	scanf("%d %d %d",&x,&y,&z);
+	sonuc = x * y * z;
+	printf("%d",sonuc);
+
+	return 0;
+}
